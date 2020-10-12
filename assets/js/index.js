6511586f946ae718365ece210d9b557a6f0b8ad8
xiaojing520-ai <542258713@qq.com>
2020-10-12 20:42:44 +0800

    个人中心基本资料的重置功能完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,46 @@
 $(function () {
-  $.ajax({
-    type: "GET",
-    url: "/my/userinfo",
-    // headers: {
-    //   Authorization:
-    //     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQyNTYsInVzZXJuYW1lIjoieHVqaW5nIiwicGFzc3dvcmQiOiIiLCJuaWNrbmFtZSI6IumdmemdmSIsImVtYWlsIjoieHh4QGpqLmNvbSIsInVzZXJfcGljIjoiIiwiaWF0IjoxNjAyMjQ5NDIwLCJleHAiOjE2MDIyODU0MjB9.HjdLEjG1JbKkTqsS6WvGvWCyrDIH_9sIQY-LvQr7qKg"
-    // },
-    success: function (res) {
-      console.log(res)
-      if (res.status == 0) {
-        var nickname =
-          res.data.nickname == "" ? res.data.username : res.data.nickname
+  getUserInfo()
+  function getUserInfo() {
+    $.ajax({
+      type: "GET",
+      url: "/my/userinfo",
+      // headers: {
+      //   Authorization:
+      //     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTQyNTYsInVzZXJuYW1lIjoieHVqaW5nIiwicGFzc3dvcmQiOiIiLCJuaWNrbmFtZSI6IumdmemdmSIsImVtYWlsIjoieHh4QGpqLmNvbSIsInVzZXJfcGljIjoiIiwiaWF0IjoxNjAyMjQ5NDIwLCJleHAiOjE2MDIyODU0MjB9.HjdLEjG1JbKkTqsS6WvGvWCyrDIH_9sIQY-LvQr7qKg"
+      // },
+      success: function (res) {
+        console.log(res)
+        if (res.status == 0) {
+          var nickname =
+            res.data.nickname == "" ? res.data.username : res.data.nickname
 
-        $(".userInfo .welcome").html(`欢迎&nbsp;&nbsp;${nickname}`)
+          $(".userInfo .welcome").html(`欢迎&nbsp;&nbsp;${nickname}`)
 
-        //如果有头像和没头像的判断
-        if (!res.data.user_pic) {
-          //没头像
-          $(".userInfo .text-avatar").html(nickname.slice(0, 1).toUpperCase())
-          $(".layui-header .text-avatar").html(
-            nickname.slice(0, 1).toUpperCase()
-          )
-        } else {
-          //有头像
-          $(".userInfo img").show().attr("src", res.data.user_pic).prev().hide()
-          $(".layui-nav img")
-            .show()
-            .attr("src", res.data.user_pic)
-            .prev()
-            .hide()
+          //如果有头像和没头像的判断
+          if (!res.data.user_pic) {
+            //没头像
+            $(".userInfo .text-avatar").html(nickname.slice(0, 1).toUpperCase())
+            $(".layui-header .text-avatar").html(
+              nickname.slice(0, 1).toUpperCase()
+            )
+          } else {
+            //有头像
+            $(".userInfo img")
+              .show()
+              .attr("src", res.data.user_pic)
+              .prev()
+              .hide()
+            $(".layui-nav img")
+              .show()
+              .attr("src", res.data.user_pic)
+              .prev()
+              .hide()
+          }
         }
       }
-    }
-  })
+    })
+  }
+  window.getUserInfo = getUserInfo
 
   // - 1.给退出按钮注册事件
   // - 2.删除本地存储中的token
--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,18 +1,64 @@
 $(function () {
   // 1. 发送ajax请求 获取当前登陆的用户的数据
   // 1.1 直接发送ajax请求
-  $.ajax({
-    type: "GET",
-    url: "/my/userinfo",
-    success: function (res) {
-      console.log(res)
-      if (res.status == 0) {
-        // 1.2 渲染页面  注意：form标签上加 lay-filter="myForm"
-        layui.form.val("myForm", res.data)
+  renderInfo()
+  function renderInfo() {
+    $.ajax({
+      type: "GET",
+      url: "/my/userinfo",
+      success: function (res) {
+        console.log(res)
+        if (res.status == 0) {
+          // 1.2 渲染页面  注意：form标签上加 lay-filter="myForm"
+          layui.form.val("myForm", res.data)
+
+          // 将获取到的数据存储起来
+          window.renderData = res.data
+        }
+      }
+    })
+  }
+
+  // 2. 实现表单验证
+  layui.form.verify({
+    nickname: function (value, item) {
+      //value：表单的值、item：表单的DOM对象
+      if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+        return "用户名不能有特殊字符"
+      }
+      if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+        return "用户名首尾不能出现下划线'_'"
+      }
+      if (/^\d+\d+\d$/.test(value)) {
+        return "用户名不能全为数字"
       }
     }
   })
 
+  //给form表单 绑定submit事件   提交修改
+  $(".myForm").on("submit", function (e) {
+    e.preventDefault()
 
-  
+    $.ajax({
+      type: "POST",
+      url: "/my/userinfo",
+      data: layui.form.val("myForm"),
+      success: function (res) {
+        console.log(res)
+        if (res.status == 0) {
+          layer(res.message)
+          renderInfo()
+          window.parent.getUserInfo()
+        }
+      }
+    })
+  })
+
+  // 给重置按钮注册事件 使表单恢复之前的值
+  $(".myForm .btn-reset").on("click", function (e) {
+    e.preventDefault()
+
+    //  所谓重置就是重新显示原来的数据
+    layui.form.val("myForm", window.renderData)
+  })
 })
