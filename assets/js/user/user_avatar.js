aaf56310de64a95ded848017cfe13e691023419b
xiaojing520-ai <542258713@qq.com>
2020-10-12 21:46:00 +0800

    更新头像之预览头像成功

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -11,4 +11,20 @@ $(function () {
 
   // 1.3 创建裁剪区域
   $image.cropper(options)
+
+  // 给上传按钮注册事件，弹出选择图片的窗口
+  $(".btn-upload").on("click", function () {
+    $("#avatar").click()
+  })
+
+  // 文件窗口弹出后，给文件按钮绑定一个change事件
+  $("#avatar").on("change", function () {
+    // 3.2 获取待上传的图片
+    var avatar = this.files[0]
+    // 3.3 生成一个链接
+    var imgUrl = URL.createObjectURL(avatar)
+
+    // 3.4 显示到img标签内
+    $image.cropper("destroy").attr("src", imgUrl).cropper(options)
+  })
 })
