57de48b693b4ee5631719e429f4ac0ee5bf9901b
xiaojing520-ai <542258713@qq.com>
2020-10-13 17:20:09 +0800

    文章分类页弹出层的数据检验完成

--- assets/js/article/article_category.js
+++ assets/js/article/article_category.js
@@ -22,4 +22,21 @@ $(function () {
       area: "520px" // 弹出框的宽度
     })
   })
+
+  // 3.实现验证功能
+  var form = layui.form
+  form.verify({
+    username: function (value, item) {
+      //value：表单的值、item：表单的DOM对象
+      if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+        return "用户名不能有特殊字符"
+      }
+      if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+        return "用户名首尾不能出现下划线'_'"
+      }
+      if (/^\d+\d+\d$/.test(value)) {
+        return "用户名不能全为数字"
+      }
+    }
+  })
 })
