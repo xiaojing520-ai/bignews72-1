236753fff6a9e60308dee9d58f2e6b70597c91e8
xiaojing520-ai <542258713@qq.com>
2020-10-14 14:07:17 +0800

    文章发布页面的图片预览功能完成

--- assets/js/article/article_release.js
+++ assets/js/article/article_release.js
@@ -16,7 +16,7 @@ $(function () {
   // 创建裁剪区
   $image.cropper(options)
 
-  // 文章分类下拉框数据的渲染
+  // 3、文章分类下拉框数据的渲染
   $.ajax({
     type: "GET",
     url: "/my/article/cates",
@@ -30,5 +30,24 @@ $(function () {
         layui.form.render()
       }
     }
+  })
+
+  $(".btn-upload").on("click", function () {
+    $("#avatar").click()
+  })
+
+  // 5. 实现图片的本地预览功能
+  // 5.1 给input标签注册change事件
+  $("#avatar").on("change", function () {
+    // 5.2 获取待上传的图片
+    var file = this.files[0]
+    // 5.3 生成图片的链接
+    var imgUrl = URL.createObjectURL(file)
+
+    // 5.4 实现本地预览功能 需要先销毁之前的 然后再显示新的
+    $("#image")
+      .cropper("destroy") // 销毁旧的裁剪区域
+      .attr("src", imgUrl) // 重新设置图片路径
+      .cropper(options) // 重新初始化裁剪区域
   })
 })
--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -20,11 +20,41 @@ $(function () {
   // 文件窗口弹出后，给文件按钮绑定一个change事件
   $("#avatar").on("change", function () {
     // 3.2 获取待上传的图片
-    var avatar = this.files[0]
+    var file = this.files[0]
     // 3.3 生成一个链接
-    var imgUrl = URL.createObjectURL(avatar)
+    var imgUrl = URL.createObjectURL(file)
 
     // 3.4 显示到img标签内
-    $image.cropper("destroy").attr("src", imgUrl).cropper(options)
+
+    $image
+      .cropper("destroy") // 销毁旧的裁剪区域
+      .attr("src", imgUrl) // 重新设置图片路径
+      .cropper(options) // 重新初始化裁剪区域
+  })
+
+  // 上传头像
+  $(".btn-sure").on("click", function (e) {
+    e.preventDefault()
+    // 4.2 生成base64格式的图片链接
+    var dataURL = $image
+      .cropper("getCroppedCanvas", {
+        // 创建一个 Canvas 画布
+        width: 100,
+        height: 100
+      })
+      .toDataURL("image/png") // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    $.ajax({
+      type: "POST",
+      url: "/my/update/avatar",
+      data: {
+        avatar: dataURL
+      },
+      success: function (res) {
+        if (res.status == 0) {
+          layer.msg(res.message)
+          window.parent.getUserInfo()
+        }
+      }
+    })
   })
 })
